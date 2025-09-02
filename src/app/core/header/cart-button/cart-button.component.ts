15bbe9493db329850331af13bb9dd5353350f4ab
Rafael Souza <rafaelsnt123@gmail.com>
2025-09-02 00:44:57 -0300

    feat(header): implement base structure and styles
    Closes #1

---
+++ src/app/core/header/cart-button/cart-button.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-button',
+  templateUrl: './cart-button.component.html',
+  styleUrls: ['./cart-button.component.scss']
+})
+export class CartButtonComponent {
+  @Input() itemCount: number = 0;
+}
